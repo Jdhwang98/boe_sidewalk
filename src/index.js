c6bdca2e1414bb64b6f5fe9347ab01def5e90518
Jose <104174192+J-APV@users.noreply.github.com>
2024-08-30 15:00:25 -0700

    second commit

--- src/index.js
+++ src/index.js
@@ -18,7 +18,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-// john_test
-// another one
-//sdfesfesf
-// main
